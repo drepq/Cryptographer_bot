9e709a48553c597af35dafad47f0f7286252e601
drepq <zloidemid@gmail.com>
2026-06-17 16:39:48 +0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from dotenv import load_dotenv
 import os
+from aiogram import Dispatcher, Bot
+from aiogram.types import message
+import asyncio
+from handler import router
+
 
 load_dotenv()
-
 API_TOKEN = os.environ.get("API_TOKEN")
+
+dp = Dispatcher()
+
+
+async def main():
+    bot = Bot(token=API_TOKEN)
+    print("Bot started")
+    dp.include_router(router)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        print("Bot stopped")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
