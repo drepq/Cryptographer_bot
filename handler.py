ea1f28b3e15d54ab977e8f34e22912aa21b6e79b
drepq <zloidemid@gmail.com>
2026-06-21 04:06:44 +0300

    work

--- handler.py
+++ handler.py
@@ -1,11 +1,17 @@
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram import types
 import base64
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
 
 router = Router()
 
+wait_users = {
+# 345325235: "encode_base64"
+}
 
 kb = types.ReplyKeyboardMarkup(keyboard=[[types.KeyboardButton(text="encode Base64"),
       types.KeyboardButton(text="decode Base64")]
@@ -27,5 +33,13 @@ async def encode_base64(message: Message):
 @router.message(F.text.lower() == "decode base64")
 async def decode_base64(message: Message):
     text = message.text
+
     decode_text = base64.b64decode(text).decode('utf-8',  errors='ignore')
     await message.answer(decode_text)
+
+@router.message(F.text)
+async def check(message: Message):
+    id = message.from_user.id
+
+    if id in wait_users.keys:
+        pass
